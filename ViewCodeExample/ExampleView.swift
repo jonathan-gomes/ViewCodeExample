f6893336402fb5e014ac45187c408054a546d37c
Jonathan Gomes <jonathangomes.ti@gmail.com>
2022-06-09 11:13:32 -0300

    separeting view from view controller

---
+++ ViewCodeExample/ExampleView.swift
@@ -0,0 +1,62 @@
+//
+//  ExampleView.swift
+//  ViewCodeExample
+//
+//  Created by Jonathan dos Santos Gomes on 09/06/22
+//
+
+import UIKit
+
+class ExampleView: UIView {
+    var button: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Change", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    var label: UILabel = {
+        let label = UILabel()
+        label.text = "default text"
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    // MARK: - init
+    override init(frame: CGRect = .zero) {
+        super.init(frame: frame)
+        self.setupViewCodeElements()
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    @objc func buttonTap() {
+        label.text = label.text == "button tapped" ? "default text" : "button tapped"
+    }
+}
+
+extension ExampleView {
+    func setupViewCodeElements() {
+        self.backgroundColor = .white
+        
+        button.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(buttonTap)))
+        self.addSubview(button)
+        self.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor
+                .constraint(equalTo: self.centerXAnchor, constant: 0),
+            label.centerYAnchor
+                .constraint(equalTo: self.centerYAnchor, constant: 0),
+            label.widthAnchor.constraint(equalToConstant: 200),
+            label.heightAnchor.constraint(equalToConstant: 30),
+            
+            button.centerXAnchor
+                .constraint(equalTo: self.centerXAnchor, constant: 0),
+            button.topAnchor.constraint(equalTo: self.label.bottomAnchor, constant: 10),
+            button.widthAnchor.constraint(equalToConstant: 200),
+            button.heightAnchor.constraint(equalToConstant: 50),
+        ])
+    }
+}
